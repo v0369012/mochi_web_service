093708c46fe3055db58ca827f6f018163a5f8457
v0369012 <v0369012@gmail.com>
2021-04-21 18:09:53 +0800

    update ancom table download button

--- server.R
+++ server.R
@@ -23011,7 +23011,7 @@ server <- function(session, input, output) {
       
       output$word_ancom_plotly_demo <- renderUI({
         
-        h3("ANCOM Volcano Plot (", input$ANCOM_level_demo, ")",
+        h3("ANCOM Volcano Plot (", "Order", ")",
            style = "color: black;top: 10px;")
       })
       
@@ -28238,7 +28238,7 @@ server <- function(session, input, output) {
       
       output$word_ancom_plotly_demo <- renderUI({
         
-        h3("ANCOM Volcano Plot (", input$ANCOM_level_demo, ")",
+        h3("ANCOM Volcano Plot (", "Family", ")",
            style = "color: black;top: 10px;")
       })
       
@@ -33466,7 +33466,7 @@ server <- function(session, input, output) {
       
       output$word_ancom_plotly_demo <- renderUI({
         
-        h3("ANCOM Volcano Plot (", input$ANCOM_level_demo, ")",
+        h3("ANCOM Volcano Plot (", "Genus", ")",
            style = "color: black;top: 10px;")
       })
       
--- ui.R
+++ ui.R
@@ -1538,11 +1538,12 @@ shinyUI(
                                             dataTableOutput(outputId = "ancom_sig"),
                                             type = spinner_type, 
                                             color.background = spinner_bg_color
-                                          )
-                                          
-                                          ) %>% shinyjs::hidden(),
+                                          ),
                                           downloadButton(outputId = "ancom_table_download",
-                                                         label = "Download the ANCOM result table (Contain all species)") %>% shinyjs::hidden()
+                                                         label = "Download the ANCOM result table (Contain all species)")
+                                          
+                                          ) %>% shinyjs::hidden()
+                                          
 
                                         
                                         
@@ -2487,11 +2488,12 @@ shinyUI(
                                                    dataTableOutput(outputId = "ancom_sig_demo"),
                                                    type = spinner_type, 
                                                    color.background = spinner_bg_color
-                                                 )
+                                                 ),
+                                                 downloadButton(outputId = "ancom_table_download_demo",
+                                                                label = "Download the ANCOM result table (Contain all species)")
                                                  
-                                               ),
-                                               downloadButton(outputId = "ancom_table_download_demo",
-                                                              label = "Download the ANCOM result table (Contain all species)")
+                                               )
+                                               
                                                
                                                
                                                
@@ -2800,7 +2802,7 @@ shinyUI(
     tags$footer(
       tags$span(
         tags$a("Molecular Bioinformatics Lab,", href = "https://fullofbeans.nctu.edu.tw/?page_id=333&lang=en", style = "color: white", target = "_blank"), 
-        " National Yang Ming Chiao Tung University, Taiwan 300, R.O.C.,", " last updated on 04/17/2021"),
+        " National Yang Ming Chiao Tung University, Taiwan 300, R.O.C.,", " last updated on 04/21/2021"),
       # tags$a(href="https://ibs.nctu.edu.tw/faculty/%E9%99%B3%E4%BA%AD%E5%A6%8F/", tags$span("Contact us!"), 
       #        target = "_blank",
       #        class="externallink", 
