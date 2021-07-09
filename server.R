1a3741601799f30be9e1a091be9a9092791af94c
v0369012 <v0369012@gmail.com>
2021-07-09 12:45:13 +0800

    ANCOM plot tile add metadata

--- server.R
+++ server.R
@@ -18259,7 +18259,10 @@ server <- function(session, input, output) {
     
     output$word_ancom_plotly <- renderUI({
       
-      h3("ANCOM Volcano Plot (", isolate({input$ANCOM_level}), ")",
+      h3("ANCOM Volcano Plot (", 
+         isolate({input$metadata_ANCOM}),
+         "-",
+         isolate({input$ANCOM_level}), ")",
          style = "color: black;top: 10px;")
     })
     
